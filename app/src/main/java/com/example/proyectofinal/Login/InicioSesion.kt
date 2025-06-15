f25c01805b90eec51e2bf0171961a565bf6cce30
Julian Esparza <117664575+JulianEsp19@users.noreply.github.com>
2025-06-14 23:48:29 -0600

    Programacion de InicioSesion.kt y Registro.kt

--- app/src/main/java/com/example/proyectofinal/Login/InicioSesion.kt
+++ app/src/main/java/com/example/proyectofinal/Login/InicioSesion.kt
@@ -1,13 +1,29 @@
 package com.example.proyectofinal.Login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyectofinal.MainActivity
 import com.example.proyectofinal.R
+import com.example.proyectofinal.objetos.Usuario
+import com.google.gson.Gson
 
 class InicioSesion : AppCompatActivity() {
+
+    //iniciar objetos del entorno visual
+    private lateinit var usuarioEditText: EditText
+    private lateinit var contrasenaEditText: EditText
+    private lateinit var iniciarSesionButton: Button
+    private lateinit var registrarseButton: Button
+    private lateinit var mantenerSesionCheckBox: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +33,71 @@ class InicioSesion : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        verificarSesion()
+
+        //asignar objetos del entorno visual
+        usuarioEditText = findViewById(R.id.usuarioEditText)
+        contrasenaEditText = findViewById(R.id.contrasenaEditText)
+        iniciarSesionButton = findViewById(R.id.iniciarSesionButton)
+        registrarseButton = findViewById(R.id.registrarseButton)
+        mantenerSesionCheckBox = findViewById(R.id.mantenerSesionCheckBox)
+
+        //asignar evento al boton
+        iniciarSesionButton.setOnClickListener {iniciarSesion()}
+        registrarseButton.setOnClickListener {registrarse()}
+    }
+    fun verificarSesion(){
+        val sharedPref = getSharedPreferences("usuarios", MODE_PRIVATE)
+        val mantenerSesion = sharedPref.getBoolean("mantenerSesion", false)
+        if (mantenerSesion) {
+            val usuario = sharedPref.getString("usuario", null)
+            if (usuario != null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    fun iniciarSesion() {
+        val usuario = usuarioEditText.text.toString()
+        val contrasena = contrasenaEditText.text.toString()
+
+        //validar que los campos no esten vacios
+        if (usuario.isEmpty() || contrasena.isEmpty()) {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //validar que el usuario y la contraseña sean correctos
+        val sharedPref = getSharedPreferences("usuarios", MODE_PRIVATE)
+        val json = sharedPref.getString("usuarios", null)
+        if (json != null) {
+            val usuarios = Gson().fromJson(json, Array<Usuario>::class.java)
+            for (usuarioJson in usuarios) {
+                if (usuarioJson.usuario == usuario && usuarioJson.contrasena == contrasena) {
+                    Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+                    if (mantenerSesionCheckBox.isChecked) {
+                        val editor = sharedPref.edit()
+                        editor.putBoolean("mantenerSesion", true)
+                        editor.putString("usuario", usuario)
+                        editor.apply()
+                    }
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    return
+                }
+            }
+            Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "El usuario no existe", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    fun registrarse() {
+        val intent = Intent(this, Registro::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/proyectofinal/Login/Registro.kt
+++ app/src/main/java/com/example/proyectofinal/Login/Registro.kt
@@ -1,13 +1,26 @@
 package com.example.proyectofinal.Login
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.proyectofinal.R
+import com.example.proyectofinal.objetos.Usuario
+import com.google.gson.Gson
 
 class Registro : AppCompatActivity() {
+    //iniciar objetos del entorno visual
+    private lateinit var nombreEditText: EditText
+    private lateinit var numeroTelefonoEditText: EditText
+    private lateinit var usuarioEditText: EditText
+    private lateinit var contrasenaEditText: EditText
+    private lateinit var confirmarContrasenaEditText: EditText
+    private lateinit var registrarseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +30,63 @@ class Registro : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //asignar objetos del entorno visual
+        nombreEditText = findViewById(R.id.nombreEditText)
+        numeroTelefonoEditText = findViewById(R.id.numeroTelefonoEditText)
+        usuarioEditText = findViewById(R.id.usuarioEditText)
+        contrasenaEditText = findViewById(R.id.contrasenaEditText)
+        confirmarContrasenaEditText = findViewById(R.id.confirmarContrasenaEditText)
+        registrarseButton = findViewById(R.id.registrarseButton)
+
+        //asignar evento al boton
+        registrarseButton.setOnClickListener {registrarse()}
     }
+
+    fun registrarse() {
+        val nombre = nombreEditText.text.toString()
+        val numeroTelefono = numeroTelefonoEditText.text.toString()
+        val usuario = usuarioEditText.text.toString()
+        val contrasena = contrasenaEditText.text.toString()
+        val confirmarContrasena = confirmarContrasenaEditText.text.toString()
+
+        //validar que los campos no esten vacios
+        if (nombre.isEmpty() || numeroTelefono.isEmpty() || usuario.isEmpty() || contrasena.isEmpty() || confirmarContrasena.isEmpty()) {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //validar que las contraseñas sean iguales
+        if (contrasena != confirmarContrasena) {
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //guardar los datos del usuario en shared preferences en json
+        val sharedPref = getSharedPreferences("usuarios", MODE_PRIVATE)
+        val json = sharedPref.getString("usuarios", null)
+        val editor = sharedPref.edit()
+
+        if (json != null) {
+            val usuariosJson = Gson().fromJson(json, Array<Usuario>::class.java).toMutableList()
+            for (usuarioJson in usuariosJson) {
+                if (usuarioJson.usuario == usuario) {
+                    Toast.makeText(this, "El usuario ya existe", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+
+            usuariosJson.add(Usuario(nombre, numeroTelefono, usuario, contrasena))
+            editor.putString("usuarios", Gson().toJson(usuariosJson))
+            editor.apply()
+            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
+            finish()
+        }else{
+            editor.putString("usuarios", Gson().toJson(mutableListOf(Usuario(nombre, numeroTelefono, usuario, contrasena))))
+            editor.apply()
+            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
 }
